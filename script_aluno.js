0ad2b24aa1340636f1d6b6eea1295d3d46f8466d
Tiago <tiago.4lexandre@gmail.com>
2024-09-06 12:05:36 -0300

    Botão de rolagem adicionado

--- script_aluno.js
+++ script_aluno.js
@@ -9,3 +9,29 @@ document.addEventListener ('DOMContentLoaded', () =>{
         sidebar.classList.toggle('active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    // Verificar se o botão foi encontrado
+    if (!scrollToTopButton) {
+        console.error('Botão de rolar para o topo não foi encontrado.');
+        return;
+    }
+
+    // Mostra o botão quando o usuário rola para baixo
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {  // Ajuste de valor para quando o botão aparecer
+            scrollToTopButton.classList.add('show');
+            scrollToTopButton.classList.remove('hide');
+        } else {
+            scrollToTopButton.classList.add('hide');
+            scrollToTopButton.classList.remove('show');
+        }
+    });
+
+    //Rolar para o topo quando o botão é clicado
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth'});
+    });
+});
